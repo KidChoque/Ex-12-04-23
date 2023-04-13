e903616520ca6e180223962530245a624a997224
KidChoque <lucassmachado1998@gmail.com>
2023-04-13 10:53:13 -0300

    Feito exercício 1

---
+++ ex-1/Program.cs
@@ -0,0 +1,23 @@
+﻿//1 - Ler o ano atual e o ano de nascimento de uma pessoa. Escrever uma mensagem que diga se
+//ela poderá ou não votar este ano (não é necessário considerar o mês em que a pessoa nasceu).
+
+int ano = DateTime.Now.Year;
+
+Console.WriteLine(ano);
+
+
+Console.WriteLine($"Digite sua idade :");
+
+int idade = int.Parse(Console.ReadLine());
+
+if ( idade < 16 )
+{
+    Console.WriteLine($"Ainda não vota");
+    
+}
+else
+{
+    Console.WriteLine($"Ja pode votar");
+    
+}
+
