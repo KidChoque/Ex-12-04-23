cf997eb3de4b330a25bf2dbc3408eff255e27b10
KidChoque <lucassmachado1998@gmail.com>
2023-04-30 00:08:40 -0300

    finalizado os exercícios do dia 12/04/23

---
+++ ex-4/Program.cs
@@ -0,0 +1,36 @@
+﻿// // 4 - Faça um programa que leia 10 valores digitados pelo usuário e no final, escreva o maior e o
+// // menor valor lido.
+
+int[] numero =new int[10];
+
+for (var i = 0; i < numero.Length ; i++)
+{
+    Console.WriteLine($"Digitew um número");
+    numero[i] = int.Parse(Console.ReadLine());
+     
+    
+}
+
+int maior = 0;
+int menor = 0;
+
+for (var i = 0; i < numero.Length; i++)
+{
+    if ( i == 0)
+{
+    maior = numero[0];
+    menor = numero[0];
+}
+else if( numero[i] > maior )
+{
+ maior = numero[i];       
+}
+else if( numero[i] < menor)
+{
+    menor = numero[i];
+}
+}
+
+Console.WriteLine($"menor número = {menor}");
+Console.WriteLine($"maior número = {maior}");
+
---
+++ ex-6/Program.cs
@@ -0,0 +1,30 @@
+﻿// //6 - Escreva um algoritmo que permita a leitura dos nomes de 10 pessoas e armazene os nomes
+// //lidos em um vetor. Após isto, o algoritmo deve permitir a leitura de mais 1 nome qualquer de
+// //pessoa (para efetuar uma busca) e depois escrever a mensagem ACHEI, se o nome estiver
+// //entre os 10 nomes lidos anteriormente (guardados no vetor), ou NÃO ACHEI caso contrário.
+
+string[] nomes = new string[10];
+bool encontrado = false;
+
+for (var i = 0; i < nomes.Length; i++)
+{
+    Console.WriteLine($"Digite o {i + 1}º nome :");
+    nomes[i] = Console.ReadLine()!;
+
+
+}
+
+Console.WriteLine($"Digite o nome a ser pesquisado");
+string pesquisa = Console.ReadLine();
+
+foreach (string nome in nomes)
+{
+    if (nome == pesquisa)
+    {
+        encontrado = true;
+        break;
+    }
+}
+
+
+Console.WriteLine(encontrado == true ? "Achei" : "Não Achei");
